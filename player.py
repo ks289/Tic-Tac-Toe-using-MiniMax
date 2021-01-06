7aa1e2db3e953d24d17678ff1a7729da548f536f
ks289 <ks289@st-andrews.ac.uk>
2021-01-06 13:04:47 +0000

    tiny bug fixed with random player

--- player.py
+++ player.py
@@ -17,7 +17,7 @@ select the available move to play.
 """
 class RandomPlayer(Player):
     def __init__(self, letter):
-        super().RandomPlayer.__init__(letter)
+        super().__init__(letter)
         
     def get_move(self, game):
         square = random.choice(game.available_moves())
